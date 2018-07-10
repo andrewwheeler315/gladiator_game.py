2429ba2ed359e878255e3ea7e523fdcb3fea9dbb
andrewwheeler315 <awheeler@basecampcodingacademy.org>
2018-07-10 09:56:10 -0500

    :

--- shell.py
+++ shell.py
@@ -1,9 +1,46 @@
+from core import *
+
+
+def name_your_fighter():
+    name = input('Name your fighter. ')
+    print(name)
+    print('Welcome to the Gladiator Circle of Slaughter!')
+
+
+def choose_attack():
+    if (attacker) (attack) (defender):
+        print(damage_dealt)
+    if (attacker) (crit_attack) (defender):
+        print(crit_damage_dealt)
+
+
+def choose_pass():
+
+
+def choose_quit():
+
+
+def choose_heal():
+    if gladiator (heal):
+        print('You restored 5 points of health. ')
+
+
+def choose_your_action():
+    choice = input('.....Attack'
+                   '.....Pass'
+                   '.....Quit'
+                   '.....Heal')
+    return choice
+
+
 def is_dead():
-    print('You are Dead')
+    if gladiator (is_dead):
+        print('You are Dead')
 
 
 def main():
 
-    
-if __name__ == __'main'__:
-    main()
+
+
+    if __name__ == '__main__':
+        main()
