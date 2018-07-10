94e7ee598b27a134052e1035a71a2983bffda407
andrewwheeler315 <awheeler@basecampcodingacademy.org>
2018-07-10 16:24:45 -0500

    :test_core.py

--- test_core.py
+++ test_core.py
@@ -2,9 +2,10 @@ from core import *
 
 
 def test_new_gladiator():
-    result = new_gladiator(100, 0, 5, 15)
+    result = new_gladiator('Jason', 100, 0, 5, 15)
     assert result == {
-        'Health': 100,
+        'name': 'Jason',
+        'health': 100,
         'rage': 0,
         'damage_low': 5,
         'damage_high': 15
@@ -12,21 +13,22 @@ def test_new_gladiator():
 
 
 def test_attack():
-    gladiator_a = new_gladiator(75, 0, 5, 15)
-    gladiator_b = new_gladiator(100, 15, 5, 15)
+    gladiator_a = new_gladiator('Jason', 75, 0, 5, 15)
+    gladiator_b = new_gladiator('Billy', 100, 15, 5, 15)
     result = attack(gladiator_a, gladiator_b)
-    assert result == gladiator_b['Health']
+    assert result == gladiator_b['health']
 
 
 def test_heal():
-    gladiator_a = new_gladiator(75, 0, 15, 30)
+    gladiator_a = new_gladiator('Jason', 75, 0, 15, 30)
     result = heal(gladiator_a)
     assert result == None
 
 
 def test_is_dead():
     assert is_dead({
-        'Health': 0,
+        'name': 'Name',
+        'health': 0,
         'rage': 30,
         'damage_low': 70,
         'damage_high': 80
