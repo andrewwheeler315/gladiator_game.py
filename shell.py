38a9831221a2e17e0f231cf168badcc8b8a87275
andrewwheeler315 <awheeler@basecampcodingacademy.org>
2018-07-11 14:04:46 -0500

    I've added three more functions that well allow you to choose a class and the weapon you would like with that class

--- shell.py
+++ shell.py
@@ -21,6 +21,32 @@ def get_choice():
             print('invalid input')
 
 
+def show_classes():
+    'Assassin' = new_gladiator(150, 0, 5, 25)
+    'Barbarian' = new_gladiator(300, 50, 15, 25)
+    'Samurai' = new_gladiator(200, 0, 15, 35)
+    'Archer' = new_gladiator(200, 25, 20, 25)
+    'White Mage' = new_gladiator(220, 22, 15, 20)
+    'Dovakiin' = new_gladiator(400, 50, 25, 50)
+    'Black Mage' = new_gladiator(250, 30, 15, 30)
+    class_choice = ['Assassin', 'Barbarian', 'Samurai', 'Archer', 
+    'White Mage', 'Dovakiin', 'Black Mage']
+
+def choose_class():
+    if user_a
+
+
+def show_weapons():
+    'Deadric Dagger' 
+    'Leviathan Axe'
+    'Meteorite Katana'
+    "Auriel's Bow"
+    'Mythril Staff'
+    'Mehrunes Razor'
+    'Materia'
+    weapon_choice = ['Deadric Dagger', 'Leviathan', 'Meteorite Katana', 
+    "Auriel's Bow", 'Mythril Staff', 'Mehrunes Razor', 'Materia']
+
 def show_gladiators(attacker, defender):
     print('''ATTACKER: {}, Health: {}, Rage: {}
     DEFENDER: {}, Health: {}, Rage: {}'''
@@ -28,14 +54,27 @@ def show_gladiators(attacker, defender):
                   defender['name'], defender['health'], defender['rage']))
 
 
+def battle(name_1, gladiator_1, name_2, gladiator_2):
+    while True:
+        if is_dead(gladiator_1) == True:
+            print(name_2.upper(), 'Wins... Fatality!')
+            break
+            exit()
+
+        if is_dead(gladiator_2) == True:
+            print(name_1.upper(), 'Wins... Fatality!')
+            break
+            exit()
+
+
 def main():
     name_1 = input('Name your fighter Player 1. ')
     gladiator_1 = core.new_gladiator(name_1, 100, 10, 15, 25)
-    print('Welcome to the Gladiator Circle of Slaughter,', (name_1))
+    print('Welcome to Mortal Kombat,', (name_1))
 
     name_2 = input('Name your fighter Player 2. ')
     gladiator_2 = core.new_gladiator(name_2, 100, 10, 15, 25)
-    print('Welcome to the Gladiator Circle of Slaughter,', (name_2))
+    print('Welcome to Mortal Kombat,', (name_2))
 
     while not (core.is_dead(gladiator_1) or core.is_dead(gladiator_2)):
         show_gladiators(gladiator_1, gladiator_2)
@@ -49,10 +88,7 @@ def main():
             print(gladiator_1['name'], 'Heals!')
             core.heal(gladiator_1)
         elif player_1_choice == 'Quit':
-            print(gladiator_1['name'], 'Sacrifices himself.')
-        elif player_1['health'] == 0:
-            print(gladiator_1['name'], 'is Dead!')
-            core.is_dead(gladiator_1)
+            print(gladiator_2['name'], 'Wins... Quitality.')
             exit()
 
         show_gladiators(gladiator_2, gladiator_1)
@@ -66,10 +102,7 @@ def main():
             print(gladiator_2['name'], 'Heals!')
             core.heal(gladiator_2)
         elif player_2_choice == 'Quit':
-            print(gladiator_2['name'], 'Sacrifices himself.')
-        elif player_2['health'] == 0:
-            print(gladiator_2['name'], 'is Dead!')
-            core.is_dead(gladiator_2)
+            print(gladiator_1['name'], 'Wins... Quitality.')
             exit()
 
 
